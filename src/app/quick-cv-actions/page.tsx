adbb4a2726db22dd14113ff32f393ef133cf9f62
Peramanathan Sathyamoorthy <sathyam.peram@gmail.com>
2025-09-14 17:42:19 +0200

    Fix the right directive quick-cv-actions

--- src/app/quick-cv-actions/page.tsx
+++ src/app/quick-cv-actions/page.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import { useState } from 'react';
 import QuestionAnswer from "@/components/question-answer";
 import { getFeatureDisclaimer, isFeatureInDevelopment } from "@/config/feature-flags";
