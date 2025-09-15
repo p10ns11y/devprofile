54b2551ab94cb3607b5e6d3b88649878368b8e6c
Peramanathan Sathyamoorthy <sathyam.peram@gmail.com>
2025-09-15 21:01:32 +0200

    Add vercel toolbar for testing feature flags in dev env

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
+import withVercelToolbar from '@vercel/toolbar/plugins/next';
+
 const nextConfig = {
   images: {},
   transpilePackages: ['@react-pdf/renderer'],
 }
 
-export default nextConfig
+export default withVercelToolbar()(nextConfig)
