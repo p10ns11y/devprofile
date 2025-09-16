f081e9da2df9d9881ad7a714afaea54d93a66b40
Peramanathan Sathyamoorthy <sathyam.peram@gmail.com>
2025-09-16 13:47:15 +0200

    Add main landmark for semantic HTML accessibility

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,9 @@ export default function RootLayout({
         <SpeedInsights />
         <SWRegister />
         {shouldInjectToolbar && <VercelToolbar />}
-        {children}
+        <main>
+          {children}
+        </main>
       </body>
     </html>
   )
